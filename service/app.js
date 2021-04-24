f51da4563dfe39b2659c7b1b420ce5f2c4dd6915
matteorapa <matteo.rapa.18@um.edu.mt>
2021-04-24 19:12:24 +0200

    Add yaml file for client

--- service/app.js
+++ service/app.js
@@ -7,8 +7,12 @@ const app = express();
 //app.use(express.static("public"));
 
 app.get('/generate', (req, res) => {
-    const amountToGenerate = 100
-    const max = 10000
+    // Each instance will generate 1,000 random numbers
+    const amountToGenerate = 1000
+
+    // The maximum random number that can be generated is 100,000
+    const max = 100000
+
     let numbers = []
     let largest = 0
     let smallest = max
