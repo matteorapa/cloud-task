fc9cafee5f77cdc5345b174e479b06cf2f1ae261
matteorapa <matteo.rapa.18@um.edu.mt>
2021-04-27 00:14:56 +0200

    Add fetch methods and request methods

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,77 @@
 import {useState, useEffect} from 'react'
+import { nanoid } from 'nanoid'
 
 function App() {
   const [numbers, setNumbers] = useState([])
-  const [largest, setLargest] = useState(53439)
-  const [smallest, setSmallest] = useState(4)
+  const [largest, setLargest] = useState(100000)
+  const [largestInstance, setLargestInstance] = useState('')
+  const [smallest, setSmallest] = useState(0)
+  const [smallestInstance, setSmallestInstance] = useState('')
+  const [client_id, setClientId] = useState(nanoid())
 
-  const sendRequests = () => {
+  const generateBatch = (cb) => {
 
-    console.log("Clicked the generate button!");
     // fetch request to 10 instances concurrently for numbers (1k each)
-    fetch("http://...")
-    .then( res => {
+
+    fetch("https://generation-dot-cis3111-cloud-computing-gae.ew.r.appspot.com/generate", {
+      method: 'POST',
+      headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          client_id: client_id
+      })
+    }).then( res => {
       return res.json()
     })
     .then((data) => {
-      setNumbers(data)
+      setNumbers(data.numbers)
+      cb(data)
     })
     .catch(error => {
       console.warn(error)
     })
+  }
+
+  const sendRequests = () => {
+
+    console.log("Clicked the generate button!");
+    let callbacks_recieved = 0
+
+    for (let i = 0; i < 10; i++) {
+      generateBatch((data)=>{
+        callbacks_recieved++
+        // console.log(data)
+        if(callbacks_recieved == 10){
+          // Request max and min generated numbers
+
+          fetch("https://generation-dot-cis3111-cloud-computing-gae.ew.r.appspot.com/maxmin", {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                client_id: client_id
+            })
+          }).then( res => {
+            return res.json()
+          })
+          .then((data) => {
+            setSmallest(data.min)
+            setLargest(data.max)
+            setSmallestInstance(data.min_instance)
+            setLargestInstance(data.max_instance)
+          })
+          .catch(error => {
+            console.warn(error)
+          })
+        }
+      })
+    }
+    
+    
   }
 
   return (
@@ -26,13 +79,14 @@ function App() {
       <span className="author">Matteo Rapa | <a href="https://github.com/matteorapa/gcloud-task">CIS3111 Cloud Computing Assignment</a></span>
 
       <div className="spacing">
+        <code>client_id: {client_id}</code>
         <h1>Generate <strong className="number">10,000</strong> random numbers.</h1>
         <button className="generate-btn" onClick={()=> sendRequests() }>GENERATE</button>
       </div>
 
       <div className="spacing">
-        <h2>The largest generated number is <u>{largest}</u>.</h2>
-        <h2>The smallest generated number is <u>{smallest}</u>.</h2>
+        <h2>The largest generated number is <u>{largest}</u>. <code>{largestInstance}</code></h2>
+        <h2>The smallest generated number is <u>{smallest}</u>. <code>{smallestInstance}</code></h2>
       </div>
 
       <div className="spacing">
